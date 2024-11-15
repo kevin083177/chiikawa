09369053248dab24aea16e64f52c7b3dfd94fc5c
kevin083177 <kevin083177@gmail.com>
2024-11-15 23:54:00 +0800

    add feature that allows user to complete the quiz then automatically play music

--- src/pages/Fail.tsx
+++ src/pages/Fail.tsx
@@ -1,3 +1,5 @@
+import fail_audio from "../assets/audio/fail.mp3";
+
 // @ts-ignore
 export const Fail = ({score}) => {
     return(
@@ -6,6 +8,7 @@ export const Fail = ({score}) => {
           <div className='container'>
             <h2>您的得分為: {score}分</h2>
             <h3>哥布林大軍朝你襲來</h3>
+            <audio src={ fail_audio } autoPlay></audio>
             <div className='btn-container'>
               <a className='btn' href='#/quiz_question'>再來一次</a>
               <a className='btn' href='#/'>回到主頁</a>
--- src/pages/Pass.tsx
+++ src/pages/Pass.tsx
@@ -1,3 +1,5 @@
+import pass_audio from "../assets/audio/pass.mp3";
+
 // @ts-ignore
 export const Pass = ({score}) => {
     return(
@@ -6,6 +8,7 @@ export const Pass = ({score}) => {
           <div className='container'>
             <h2>您的得分為: {score}分</h2>
             <h3>呀~~~~哈</h3>
+            <audio src={ pass_audio } autoPlay></audio>
             <div className='btn-container'>
               <a className='btn' href='#/quiz_question'>再來一次</a>
               <a className='btn' href='#/'>回到主頁</a>
